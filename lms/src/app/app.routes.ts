0eb3f90b76fc413fa1a7846a893402f346889c2a
Tanisha Pavecha <144556233+TanishaPavecha@users.noreply.github.com>
2025-05-13 08:35:56 +0530

    edit

--- lms/src/app/app.routes.ts
+++ lms/src/app/app.routes.ts
@@ -6,17 +6,17 @@ import { SignupComponent } from './signup/signup.component';
 import { CourseCategoriesComponent } from './course-categories/course-categories.component';
 import { BecomeMemberComponent } from './become-member/become-member.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
-
 import { CoursesComponent } from './course-categories/courses/courses.component';
+import { AuthLoginGuard } from './authlogin.guard';
 
 export const routes: Routes = [
-    { path: 'home' , component: WelcomePageComponent },
-    { path: 'dashboard', component: StudentDashboardComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: 'courses', component: CourseCategoriesComponent },
-    { path: 'courses/:category', component: CoursesComponent },
-    { path: 'become-member', component: BecomeMemberComponent},
-    { path: 'pop-up', component: PopUpComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full'} 
+  { path: 'home', component: WelcomePageComponent },
+  { path: 'dashboard', component: StudentDashboardComponent },
+  { path: 'login', component: LoginComponent, canActivate: [] },
+  { path: 'signup', component: SignupComponent, canActivate: [AuthLoginGuard] },
+  { path: 'courses', component: CourseCategoriesComponent },
+  { path: 'courses/:category', component: CoursesComponent },
+  { path: 'become-member', component: BecomeMemberComponent },
+  { path: 'pop-up', component: PopUpComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
